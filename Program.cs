202be18e755f965c29c2ac72487aca901abaf837
Chloe Nawfal <chloe.nawfal@net.usj.edu.lb>
2025-12-16 20:56:22 +0200

    Add main loop to read the expression

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter your expression : ");
+            string s = "" + Console.ReadLine();
+
+            if (string.IsNullOrEmpty(s))
+            {
+                Console.WriteLine("You didn't enter an expression.");
+            }else if (Letters(s))
+            {
+                Console.WriteLine("You entered a letter, can't give you the result.");
+            }
+            else
+            {
+                //int result = Evaluate(s)
+                //Console.WriteLine($"The result is : {result}");
+            }
+        }
+
+        static bool Letters(string s)
+        {
+            s = s.ToLower();
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i]>='a'&& s[i] <= 'z')
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
